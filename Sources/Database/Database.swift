51380b4223451bf828c138e042379500d5b5bf90
Lucas Bighi <lucas.bighi@turbobank.co>
2022-03-18 14:08:45 -0300

    first commit

--- Sources/Database/Database.swift
+++ Sources/Database/Database.swift
@@ -1,6 +1,19 @@
+import Foundation
 public struct Database {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
+    
+    public static let `default` = Database()
+    
+    public enum Key: String {
+        case hasBiometry
+        case login
+        case password
+    }
+    
+    public func set(_ value: Any, forKey key: Key) {
+        UserDefaults.standard.set(value, forKey: key.rawValue)
+    }
+    
+    public func getValue(forKey key: Key) -> Any? {
+        return UserDefaults.standard.value(forKey: key.rawValue)
     }
 }
