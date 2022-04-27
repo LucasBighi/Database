a611250aeefde3a1db69343443734df0b8def08a
Lucas Marques Bighi <lucas.bighi@turbobank.co>
2022-04-27 15:03:11 -0300

    initial commit

--- Sources/Database/Database.swift
+++ Sources/Database/Database.swift
@@ -1,13 +1,46 @@
-import Foundation
+import RealmSwift
+
 public struct Database {
     
-    public static let `default` = Database()
-    
-    public func set(_ value: Any, forKey key: DatabaseKey) {
-        UserDefaults.standard.set(value, forKey: key.key)
+    public static let `default` = try! Database()
+
+    private var realm: Realm
+
+    private init() throws {
+        self.realm = try Realm()
+    }
+
+    public func configure() {
+
+    }
+
+    public func create(_ object: Storable) {
+        let newObject = object
+        try! realm.write {
+            realm.add(newObject)
+        }
     }
     
-    public func getValue(forKey key: DatabaseKey) -> Any? {
-        return UserDefaults.standard.value(forKey: key.key)
+    public func get<T: Storable>(_ type: T.Type) -> T? {
+        return realm.objects(T.self).first
     }
+    
+    public func update<T: Storable>(_ type: T.Type, block: (T) -> Void) {
+        guard let toUpdateObject = get(T.self) else { return }
+        try! realm.write {
+            block(toUpdateObject)
+        }
+    }
+    
+    public func delete<T: Storable>(_ type: T.Type) {
+        guard let toDeleteObject = get(T.self) else { return }
+        try! realm.write {
+            realm.delete(toDeleteObject)
+        }
+    }
+}
+
+public protocol Storable: Object {
+    
+    
 }
